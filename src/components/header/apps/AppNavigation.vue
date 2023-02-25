85b30c2996891934d21dcc815e5f86534edbb341
Garik <Garikbarsegyan93@mail.ru>
2023-02-25 21:17:59 +0300

    Deleted сщььcomments

--- src/components/header/apps/AppNavigation.vue
+++ src/components/header/apps/AppNavigation.vue
@@ -24,21 +24,6 @@
 import AppMobileMenu from "@/components/header/apps/AppMobileMenu";
 
 export default {
-  methods: {
-    // closeMobileMenu() {
-    //   this.$store.commit("closeMobileMenu")
-    // },
-    // openMobileMenu() {
-    //   this.$store.commit("openMobileMenu")
-    // },
-
-    // skipMobileMenu(event) {
-    //   let target = event.target
-    //
-    //   if (target.tagName !== "A") return
-    //   this.closeMobileMenu()
-    // }
-  },
   components: {AppMobileMenu}
 }
 </script>
@@ -53,10 +38,8 @@ export default {
 
 .menu {
   @include flex(center);
-  padding: 18px 0;
   list-style: none;
   flex-wrap: wrap;
-  width: 100%;
   z-index: 2;
   transition: .5s all;
 
@@ -85,32 +68,6 @@ export default {
   .navigation {
     display: none;
   }
-  //.mobile-menu-wrapper {
-  //  position: fixed;
-  //  top: 0;
-  //  left: 0;
-  //  width: 100%;
-  //  height: 100vh;
-  //  text-align: center;
-  //  z-index: 4;
-  //  transition: transform 0.3s ease;
-  //  transform: translateY(-100%);
-  //  padding: 0 20px;
-  //  background-color: $colorBtnBlue;
-  //}
-
-  //.mobile-menu {
-  //  padding: 15px 0;
-  //  margin: 0;
-  //  display: flex;
-  //  flex-direction: column;
-  //  justify-content: center;
-  //  height: 100vh;
-  //
-  //  &__item {
-  //    padding: 20px;
-  //  }
-  //}
 
   .menu__item:first-child > .menu__link {
     padding-left: 10px;
@@ -119,34 +76,5 @@ export default {
   .menu__item:last-child > .menu__link {
     padding-right: 10px;
   }
-
-  .menu__link {
-    color: $colorWhite;
-
-    &:hover {
-      color: $colorBlack;
-    }
-  }
-
-  //.mobile-menu-close {
-  //  position: relative;
-  //  width: 40px;
-  //  height: 5px;
-  //  background-color: #ffffff;
-  //  transform: rotate(45deg);
-  //  margin-left: auto;
-  //  top: 30px;
-  //  cursor: pointer;
-  //
-  //  &:after {
-  //    content: "";
-  //    position: absolute;
-  //    width: 40px;
-  //    height: 5px;
-  //    background-color: #ffffff;
-  //    left: 0;
-  //    transform: rotate(-90deg);
-  //  }
-  //}
 }
 </style>
